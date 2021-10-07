111061033dc5f11f635aa7bf0db0e69ea8a697ea
Stephen Crane <scrane88@gmail.com>
2021-10-07 19:13:30 -0400

    pass model specs

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,32 @@
 class Ride < ApplicationRecord
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    if enough_tickets? && tall_enough?
+      send_user_on_ride
+    else
+      message = "Sorry."
+      message = "#{message} You do not have enough tickets to ride the Roller Coaster." unless enough_tickets?
+      message = "#{message} You are not tall enough to ride the Roller Coaster." unless tall_enough?
+      message
+    end
+  end
+
+private
+
+  def enough_tickets?
+    user.tickets > attraction.tickets
+  end
+
+  def tall_enough?
+    user.height > attraction.min_height
+  end
+
+  def send_user_on_ride
+    user.give_tickets(attraction.tickets)
+    user.update_nausea(attraction.nausea_rating)
+    user.update_happiness(attraction.happiness_rating)
+    user.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   has_many :attractions, through: :rides
 
   has_secure_password
+
+  def give_tickets(amount)
+    self.tickets -= amount
+  end
+
+  def mood
+    happiness > nausea ? "happy" : "sad"
+  end
+
+  def update_happiness(amount)
+    self.happiness += amount
+  end
+
+  def update_nausea(amount)
+    self.nausea += amount
+  end
 end
