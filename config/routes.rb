8e37ae700f0b314dcb9c87137644696bae490aa0
Stephen Crane <scrane88@gmail.com>
2021-10-07 19:32:20 -0400

    add New user form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :rides
   resources :attractions
   resources :users
+
+  root "rides#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
