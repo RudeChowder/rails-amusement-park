98c1776d3fdd6e1efd5ba84693d2928764329593
Stephen Crane <scrane88@gmail.com>
2021-10-07 19:42:51 -0400

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   def show
-
+    @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation, :height)
+    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users
 
   root "rides#index"
+  get "/signin", to: "sessions#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
