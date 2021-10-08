9bdf852556fe93d0ece14e02fa910202d64c1eb2
Stephen Crane <scrane88@gmail.com>
2021-10-07 20:03:16 -0400

    set up session and signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new
-    
+    @user = User.new
+    @users = User.all
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
   end
 end
